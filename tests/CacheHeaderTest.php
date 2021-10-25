a3564231be4f40e22f27c6998eac3af4e98a24d0
James (DPC) <james.ellis@dpc.nsw.gov.au>
2021-10-26 10:55:25 +1100

    Split up tests

--- tests/CacheHeaderTest.php
+++ tests/CacheHeaderTest.php
@@ -2,6 +2,7 @@
 
 namespace NSWDPC\Utilities\Cache\Tests;
 
+use NSWDPC\Utilities\Cache\CacheHeaderConfiguration;
 use SilverStripe\CMS\Controllers\ContentController;
 use SilverStripe\CMS\Models\SiteTree;
 use SilverStripe\Control\HTTPResponse;
@@ -41,19 +42,25 @@ class CacheHeaderTest extends FunctionalTest {
             $page->publishSingle();
         }
 
-    }
-
-    public function testCacheHeaders() {
-
         // ensure on the live stage
         Versioned::set_stage(Versioned::LIVE);
 
-        // need to update default states as framework disables in dev env
-        Config::inst()->set(HTTPCacheControlMiddleware::class, 'defaultState', 'enabled');
+        // need to update default states as framework sets defaultState: 'disabled' in dev env
+        Config::inst()->set(HTTPCacheControlMiddleware::class, 'defaultState', HTTPCacheControlMiddleware::STATE_ENABLED);
         Config::inst()->set(HTTPCacheControlMiddleware::class, 'defaultForcingLevel', 0);
 
-        // request without session
+        // The test application has a configured public state by default
+        CacheHeaderConfiguration::config()->set('state', 'public');
+        CacheHeaderConfiguration::config()->set('max_age', 7301);
+        CacheHeaderConfiguration::config()->set('s_max_age', 7401);
+        CacheHeaderConfiguration::config()->set('must_revalidate', true);
+
+        // initiall request without session
         $this->logOut();
+
+    }
+
+    public function testCacheHeaders() {
 
         // page has form, this should disableCache
         $response = $this->get("header-test/");
@@ -70,6 +77,9 @@ class CacheHeaderTest extends FunctionalTest {
         $this->assertTrue( $this->hasCacheDirective($parts, "no-cache"), "Header {$headers['cache-control']} missing no-cache" );
         $this->assertTrue( $this->hasCacheDirective($parts, "no-store"), "Header {$headers['cache-control']} missing no-store" );
         $this->assertTrue( $this->hasCacheDirective($parts, "must-revalidate"), "Header {$headers['cache-control']} missing must-revalidate" );
+    }
+
+    public function testCacheHeadersWithNoForm() {
 
         // request and do not include form
         $response = $this->get("header-test/?noform=1");
@@ -80,16 +90,21 @@ class CacheHeaderTest extends FunctionalTest {
          * the test controller has the following set
          * [0] => public
          * [1] => must-revalidate
-         * [2] => s-maxage=7201
-         * [3] => max-age=7200
+         * [2] => s-maxage=7401
+         * [3] => max-age=7301
          */
 
         $parts = explode("," , $headers['cache-control']);
         $parts = array_map("trim", $parts);
         $this->assertTrue( $this->hasCachingState($parts, HTTPCacheControlMiddleware::STATE_PUBLIC), "Header {$headers['cache-control']} should be public" );
         $this->assertTrue( $this->hasCacheDirective($parts, "must-revalidate"), "Header {$headers['cache-control']} missing must-revalidate" );
-        $this->assertTrue( $this->hasCacheDirective($parts, "s-maxage=7201"), "Header {$headers['cache-control']} missing s-maxage=7201");
-        $this->assertTrue( $this->hasCacheDirective($parts, "max-age=7200"), "Header {$headers['cache-control']} missing max-age=7200" );
+        $this->assertTrue( $this->hasCacheDirective($parts, "s-maxage=7401"), "Header {$headers['cache-control']} missing s-maxage=7401");
+        $this->assertTrue( $this->hasCacheDirective($parts, "max-age=7301"), "Header {$headers['cache-control']} missing max-age=7301" );
+
+    }
+
+    // formcache=1 -> disableSecurityToken, set method = GET
+    public function testCacheHeadersWithFormCache() {
 
         // request but turn off the security token and set to GET
         // the result should be public caching
@@ -101,16 +116,22 @@ class CacheHeaderTest extends FunctionalTest {
          * the test controller has the following set
          * [0] => public
          * [1] => must-revalidate
-         * [2] => s-maxage=7201
-         * [3] => max-age=7200
+         * [2] => s-maxage=7401
+         * [3] => max-age=7301
          */
 
         $parts = explode("," , $headers['cache-control']);
         $parts = array_map("trim", $parts);
         $this->assertTrue( $this->hasCachingState($parts, HTTPCacheControlMiddleware::STATE_PUBLIC), "Header {$headers['cache-control']} should be public" );
         $this->assertTrue( $this->hasCacheDirective($parts, "must-revalidate"), "Header {$headers['cache-control']} missing must-revalidate" );
-        $this->assertTrue( $this->hasCacheDirective($parts, "s-maxage=7201"), "Header {$headers['cache-control']} missing s-maxage=7201");
-        $this->assertTrue( $this->hasCacheDirective($parts, "max-age=7200"), "Header {$headers['cache-control']} missing max-age=7200" );
+        $this->assertTrue( $this->hasCacheDirective($parts, "s-maxage=7401"), "Header {$headers['cache-control']} missing s-maxage=7401");
+        $this->assertTrue( $this->hasCacheDirective($parts, "max-age=7301"), "Header {$headers['cache-control']} missing max-age=7301" );
+
+    }
+
+    // formcache=1 -> disableSecurityToken, set method = GET
+    // Test with form disableCache turned off but with a session
+    public function testCacheHeadersWithFormCacheLoggedIn() {
 
         // log in - disable cache
         $this->logInWithPermission('ADMIN');
--- tests/CacheHeaderTestPageController.php
+++ tests/CacheHeaderTestPageController.php
@@ -2,7 +2,6 @@
 
 namespace NSWDPC\Utilities\Cache\Tests;
 
-use NSWDPC\Utilities\Cache\CacheHeaderConfiguration;
 use SilverStripe\Core\Config\Config;
 use SilverStripe\Core\Injector\Injector;
 use SilverStripe\Dev\TestOnly;
@@ -13,18 +12,6 @@ use SilverStripe\Forms\TextField;
 use PageController;
 
 class CacheHeaderTestPageController extends PageController implements TestOnly {
-
-    public function doInit() {
-
-        // set public cache by default, as that is what we want to test
-        $inst = Injector::inst()->create(CacheHeaderConfiguration::class);
-        $inst->config()->set('state', 'public');
-        $inst->config()->set('max_age', 7200);
-        $inst->config()->set('s_max_age', 7201);
-        $inst->config()->set('must_revalidate', true);
-
-        parent::doInit();
-    }
 
     public function Form()
     {
