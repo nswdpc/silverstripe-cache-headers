c77f912467e7a392a2137189b94b3713266f0f1a
James (DPC) <james.ellis@dpc.nsw.gov.au>
2022-01-20 11:25:37 +1100

    Improve handling around content controllers with a non 'Anyone' CanViewType value

--- tests/CacheHeaderTest.php
+++ tests/CacheHeaderTest.php
@@ -14,6 +14,8 @@ use SilverStripe\Dev\FunctionalTest;
 use SilverStripe\Dev\TestOnly;
 use SilverStripe\Versioned\Versioned;
 use SilverStripe\View\SSViewer;
+use SilverStripe\SiteConfig\SiteConfig;
+use SilverStripe\Security\InheritedPermissions;
 use Page;
 
 class CacheHeaderTest extends FunctionalTest {
@@ -55,9 +57,20 @@ class CacheHeaderTest extends FunctionalTest {
         CacheHeaderConfiguration::config()->set('s_max_age', 7401);
         CacheHeaderConfiguration::config()->set('must_revalidate', true);
 
-        // initiall request without session
+        $this->setSiteConfigCanViewType( InheritedPermissions::ANYONE );
+
+        // intial request without session
         $this->logOut();
 
+    }
+
+    /**
+     * Set site config CanViewType
+     */
+    private function setSiteConfigCanViewType(string $type) {
+        $siteConfig = SiteConfig::current_site_config();
+        $siteConfig->CanViewType = $type;
+        $siteConfig->write();
     }
 
     public function testCacheHeaders() {
@@ -144,6 +157,106 @@ class CacheHeaderTest extends FunctionalTest {
         $this->assertFalse( $this->hasCachingState($parts, HTTPCacheControlMiddleware::STATE_PRIVATE), "Header {$headers['cache-control']} should be private" );
         $this->assertTrue( $this->hasCacheDirective($parts, "must-revalidate"), "Header {$headers['cache-control']} missing must-revalidate" );
 
+    }
+
+    /**
+     * Tests based on CanViewType
+     */
+    public function testCanViewRootPage() {
+        // Inherit (root page)
+        $response = $this->get("/header-test/?noform=1");
+        $headers = $response->getHeaders();
+        $this->assertTrue(!empty($headers['cache-control']), "Page 1 - no cache-control header in response");
+        $parts = $this->getCacheControlParts($headers['cache-control']);
+        $this->assertTrue( $this->hasCacheDirective($parts, "public"), "Page 1 - Header {$headers['cache-control']} missing public" );
+
+    }
+
+    public function testCanViewSubPageAnyone() {
+        // Anyone page under root should be public
+        $response = $this->get("/header-test/sub-page-header-test/?noform=1");
+        $headers = $response->getHeaders();
+        $this->assertTrue(!empty($headers['cache-control']), "Page 2 - no cache-control header in response");
+        $parts = $this->getCacheControlParts($headers['cache-control']);
+        $this->assertTrue( $this->hasCacheDirective($parts, "public"), "Page 2 - Header {$headers['cache-control']} missing public" );
+    }
+
+    public function testCanViewSubPageOnlyTheseUsers() {
+        // OnlyTheseUsers should not be public
+        $response = $this->get("/header-test/restricted-page-header-test/?noform=1");
+        $headers = $response->getHeaders();
+        $this->assertTrue(!empty($headers['cache-control']), "Page 3 - no cache-control header in response");
+        $parts = $this->getCacheControlParts($headers['cache-control']);
+        $this->assertTrue( $this->hasCacheDirective($parts, "no-cache"), "Page 3 - Header {$headers['cache-control']} missing no-cache" );
+        $this->assertTrue( $this->hasCacheDirective($parts, "no-store"), "Page 3 - Header {$headers['cache-control']} missing no-store" );
+        $this->assertTrue( $this->hasCacheDirective($parts, "must-revalidate"), "Page 3 - Header {$headers['cache-control']} missing must-revalidate" );
+    }
+
+    public function testCanViewSubPageLoggedInUsers() {
+        // LoggedInUsers page should not be public
+        $response = $this->get("/header-test/loggedin-page-header-test/?noform=1");
+        $headers = $response->getHeaders();
+        $this->assertTrue(!empty($headers['cache-control']), "Page 4 - no cache-control header in response");
+        $parts = $this->getCacheControlParts($headers['cache-control']);
+        $this->assertTrue( $this->hasCacheDirective($parts, "no-cache"), "Page 4 - Header {$headers['cache-control']} missing no-cache" );
+        $this->assertTrue( $this->hasCacheDirective($parts, "no-store"), "Page 4 - Header {$headers['cache-control']} missing no-store" );
+        $this->assertTrue( $this->hasCacheDirective($parts, "must-revalidate"), "Page 4 - Header {$headers['cache-control']} missing must-revalidate" );
+    }
+
+    public function testCanViewSubPageInheritedAnyone() {
+        // Inherited from parent page with Anyone - should bge public
+        $response = $this->get("/header-test/sub-page-header-test/inherited-anyone-page-header-test/?noform=1");
+        $headers = $response->getHeaders();
+        $this->assertTrue(!empty($headers['cache-control']), "Page 5 - no cache-control header in response");
+        $parts = $this->getCacheControlParts($headers['cache-control']);
+        $this->assertTrue( $this->hasCacheDirective($parts, "public"), "Page 5 - Header {$headers['cache-control']} missing public" );
+    }
+
+    public function testCanViewSubPageInheritedOnlyTheseUsers() {
+        // Inherited from parent page with OnlyTheseUsers set - not public
+        $response = $this->get("/header-test/restricted-page-header-test/inherited-restricted-page-header-test/?noform=1");
+        $headers = $response->getHeaders();
+        $this->assertTrue(!empty($headers['cache-control']), "Page 6 - no cache-control header in response");
+        $parts = $this->getCacheControlParts($headers['cache-control']);
+        $this->assertTrue( $this->hasCacheDirective($parts, "no-cache"), "Page 6 - Header {$headers['cache-control']} missing no-cache" );
+        $this->assertTrue( $this->hasCacheDirective($parts, "no-store"), "Page 6 - Header {$headers['cache-control']} missing no-store" );
+        $this->assertTrue( $this->hasCacheDirective($parts, "must-revalidate"), "Page 6 - Header {$headers['cache-control']} missing must-revalidate" );
+    }
+
+    /**
+     * Restrict SiteConfig to Logged In Users and test root page (Inherit)
+     */
+    public function testCanViewRestrictedRootPage() {
+        $this->setSiteConfigCanViewType( InheritedPermissions::LOGGED_IN_USERS );
+        $response = $this->get("/parentless-test/?noform=1");
+        $headers = $response->getHeaders();
+        $this->assertTrue(!empty($headers['cache-control']), "Page 7 - no cache-control header in response");
+        $parts = $this->getCacheControlParts($headers['cache-control']);
+        $this->assertTrue( $this->hasCacheDirective($parts, "no-cache"), "Page 7 - Header {$headers['cache-control']} missing no-cache" );
+        $this->assertTrue( $this->hasCacheDirective($parts, "no-store"), "Page 7 - Header {$headers['cache-control']} missing no-store" );
+        $this->assertTrue( $this->hasCacheDirective($parts, "must-revalidate"), "Page 7 - Header {$headers['cache-control']} missing must-revalidate" );
+        $this->setSiteConfigCanViewType( InheritedPermissions::ANYONE );
+    }
+
+    /**
+     * Sub Page anyone, but restricted site config - not public
+     */
+    public function testCanViewRestrictedSubPageAnyone() {
+        $this->setSiteConfigCanViewType( InheritedPermissions::LOGGED_IN_USERS );
+        $response = $this->get("/header-test/sub-page-header-test/?noform=1");
+        $headers = $response->getHeaders();
+        $this->assertTrue(!empty($headers['cache-control']), "Page 2 - no cache-control header in response");
+        $parts = $this->getCacheControlParts($headers['cache-control']);
+        $this->assertTrue( $this->hasCacheDirective($parts, "no-cache"), "Page 2 - Header {$headers['cache-control']} missing no-cache" );
+        $this->assertTrue( $this->hasCacheDirective($parts, "no-store"), "Page 2 - Header {$headers['cache-control']} missing no-store" );
+        $this->assertTrue( $this->hasCacheDirective($parts, "must-revalidate"), "Page 2 - Header {$headers['cache-control']} missing must-revalidate" );
+        $this->setSiteConfigCanViewType( InheritedPermissions::ANYONE );
+    }
+
+    private function getCacheControlParts($header) {
+        $parts = explode("," , $header);
+        $parts = array_map("trim", $parts);
+        return $parts;
     }
 
     private function hasCachingState(array $header, $state) {
