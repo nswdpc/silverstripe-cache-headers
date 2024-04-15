7b8d38cd9f8d0e6fc7fc4ab1d040f0053f1ac2ba
James (DPC) <69664712+JamesDPC@users.noreply.github.com>
2024-04-15 17:31:02 +1000

    Refactor hasAnyoneViewPermission, split out handling into methods. Set early if the required state can be determined

--- src/Extensions/ContentControllerExtension.php
+++ src/Extensions/ContentControllerExtension.php
@@ -5,6 +5,7 @@ use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\Core\Extension;
 use SilverStripe\Control\Middleware\HTTPCacheControlMiddleware;
 use SilverStripe\Security\InheritedPermissions;
+use SilverStripe\SiteConfig\SiteConfig;
 use SilverStripe\Versioned\Versioned;
 
 /**
@@ -16,38 +17,64 @@ class ContentControllerExtension extends Extension {
      * Handle actions after controller init
      */
     public function onAfterInit() {
-        // When on the live stage, determine if the page has restrictions
+        $this->applyRestrictedRecordCacheState();
+    }
+
+    /**
+     * When on the live stage, determine if the page has restrictions
+     * The draft stage defines its own cache state
+     */
+    private function applyRestrictedRecordCacheState() {
         $stage = Versioned::get_stage();
-        if($stage == Versioned::LIVE) {
-            $record = $this->owner->data();
-            if($record && ($record instanceof SiteTree) && !$this->hasAnyoneViewPermission($record)) {
-                HTTPCacheControlMiddleware::singleton()->disableCache()->useAppliedState();
-            }
+        if($stage != Versioned::LIVE) {
+            return;
+        }
+        $record = $this->owner->data();
+        if(!$record || !($record instanceof SiteTree)) {
+            // misconfiguration, disable cache
+            Logger::log("Controller has no record, calling disabledCache()", "NOTICE");
+            return $this->setDisableCacheState();
+        }
+        $siteConfig = $record->getSiteConfig();
+        if(!$siteConfig || !($siteConfig instanceof SiteConfig)) {
+            // misconfiguration, disable cache
+            Logger::log("Record is not a Siteconfig, calling disabledCache()", "NOTICE");
+            return $this->setDisableCacheState();
+        }
+        if($siteConfig->CanViewType !== InheritedPermissions::ANYONE) {
+            // restricted siteconfig setting
+            return $this->setDisableCacheState();
+        } else if(!$this->hasAnyoneViewPermission($record, $siteConfig)) {
+            // restricted sitetree record setting
+            return $this->setDisableCacheState();
         }
     }
 
     /**
+     * Disable the cache state, by calling disableCache
+     * and ensure that the state applied here is used
+     */
+    private function setDisableCacheState() {
+        HTTPCacheControlMiddleware::singleton()->disableCache(true)->useAppliedState();
+        return;
+    }
+
+    /**
      * Determine whether a SiteTree record can be viewed by anyone, taking into
-     * account site access settings and parent settings
+     * account parent settings and site config
      * @return bool
      */
-    private function hasAnyoneViewPermission(SiteTree $record) : bool {
-        // siteconfig check
-        $siteConfig = $record->getSiteConfig();
-        if($siteConfig && $siteConfig->CanViewType !== InheritedPermissions::ANYONE) {
-            return false;
-        }
+    private function hasAnyoneViewPermission(SiteTree $record, SiteConfig $siteConfig) : bool {
         if($record->CanViewType === InheritedPermissions::ANYONE) {
             // this record sets permissions
             return true;
         } else if ($record->CanViewType === InheritedPermissions::INHERIT) {
-            // inheriting from parent or site config
             if( ($parent = $record->Parent()) && $parent->exists() ) {
-                // record has parent
-                return $this->hasAnyoneViewPermission($parent);
+                // inheriting from parent
+                return $this->hasAnyoneViewPermission($parent, $siteConfig);
             } else {
-                // record has no parent, inherit from site config
-                return $siteConfig && $siteConfig->CanViewType === InheritedPermissions::ANYONE;
+                // record has no parent, site config check will pick this up
+                return $siteConfig->CanViewType === InheritedPermissions::ANYONE;
             }
         } else {
             // not
